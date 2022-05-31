697b186f253b24aee3572deac965cd172eb32cb3
ChrisLolz <335655049@gapps.yrdsb.ca>
2022-05-31 10:08:28 -0400

    added canvas and you can draw on it

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -4,15 +4,20 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Visualizer extends JFrame{
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class Visualizer extends JFrame implements MouseMotionListener{
     paintBoardPanel panel;
     
     final int MAX_X = (int)getToolkit().getScreenSize().getWidth();
     final int MAX_Y = (int)getToolkit().getScreenSize().getHeight();    
 
     Visualizer (){
+    	addMouseMotionListener(this);
+    	setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         this.panel = new paintBoardPanel();
-        this.panel.setBackground(Color.LIGHT_GRAY);
+        this.panel.setBackground(Color.WHITE);
         this.getContentPane().add(BorderLayout.CENTER, panel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(MAX_X/2, MAX_Y/2);
@@ -23,7 +28,19 @@ public class Visualizer extends JFrame{
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            this.repaint();
         }        
     }
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		Graphics g = getGraphics();
+		g.setColor(Color.BLACK);
+		g.fillOval(e.getX(), e.getY(), 20, 20);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
