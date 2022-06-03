c0b873769198853e36bf65a11c15c4df8a4fade1
kylezhengkz <95050517+kylezhengkz@users.noreply.github.com>
2022-06-03 08:57:03 -0400

    Update PaintBoard.java

--- src/PaintBoard.java
+++ src/PaintBoard.java
@@ -2,6 +2,24 @@
 public class PaintBoard {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		new Menu();
+		
+		// stalls until a button is pressed
+		while (Menu.buttonPressed == false) {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {}
+		}
+		
+		// add server functions later
+		if (Menu.serverCreate == true) {
+			new Visualizer();
+		} else if (Menu.serverJoin == true) {
+			new Visualizer();
+		}
+
 		new Visualizer();
 	}
 
