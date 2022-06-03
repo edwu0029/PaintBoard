77bcb3ef6df7f26fd0483489bfc2c736e14667a0
kylezhengkz <95050517+kylezhengkz@users.noreply.github.com>
2022-06-03 12:49:17 -0400

    Add files via upload

---
+++ src/JoinServerPanel.java
@@ -0,0 +1,48 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+public class JoinServerPanel extends JFrame implements ActionListener {
+	
+	static boolean buttonPressed = false;
+	public static String ipAddress;
+	JButton button;
+	JTextField textField;
+	
+	JoinServerPanel() {
+		
+		this.setLayout(new FlowLayout());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(800, 40)); 
+		textField.setText("Input IP Address");
+		button = new JButton("Enter IP Address");
+		button.addActionListener(this);
+		
+		this.add(button);
+		this.add(textField);
+		this.pack();
+		this.setVisible(true);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() == button) {
+			// add exceptions if IP Address is invalid
+			ipAddress = textField.getText();
+			
+			// insert exceptions here
+			
+			buttonPressed = true; // place after exceptions
+			dispose();
+		}
+		
+	}
+
+}
