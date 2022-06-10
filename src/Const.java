e73154a2d24d793f54e6c16e458d2fdcb637bbb7
kylezhengkz <95050517+kylezhengkz@users.noreply.github.com>
2022-06-10 08:59:01 -0400

    Update Const.java

--- src/Const.java
+++ src/Const.java
@@ -10,6 +10,8 @@ public class Const {
 	public static final int ADD_TEXT = 5;
 	public static final int REMOVE_TEXT = 6;
 	public static final int CLEAR = 7;
+	public static final int CHAT = 8;
+	public static final int SEND_MESSAGE = 9;
 	
 	//Tool Constants
     public static final int BRUSH = 1;
